44b784862f29103840f1f30bfab5ba869499f4c8
cobboldgit <augustinecobbold6@gmail.com>
2022-01-30 10:18:43 -0800

    learn react native basics

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Image,
+  Text
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <Image source={require("./assets/profile.jpg")} style={styles.image}/>
+        <Text style={{color: "#fff", fontSize: 30, marginVertical: 20}}>Profile</Text>
+        <View style={styles.profileContainer}>
+          <Text style={styles.profile}>Name</Text>
+          <Text style={styles.profile}>John Cobbold</Text>
+        </View>
+        <View style={styles.profileContainer}>
+          <Text style={styles.profile}>Email</Text>
+          <Text style={styles.profile}>johnCob@yahoo.com</Text>
+        </View>
+        <View style={styles.profileContainer}>
+          <Text style={styles.profile}>Gender</Text>
+          <Text style={styles.profile}>Male</Text>
+        </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
+let styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#566764",
+    justifyContent: "center",
+    alignItems: "center"    
+
   },
+
+  image: {
+    height: 300,
+    width: 300,
+    borderRadius: 150,
+  },
+
+  profileContainer : {
+    flexDirection: "row",
+  },
+
+  profile: {
+    color: "#fff",
+    fontSize: 20,
+    borderWidth: 1,
+    flex: 1,
+    borderColor: "rgb(65, 65, 65)"
+  }
+
+ 
 });
